478db7fbdee208f22bb86255edc95d8ac44f9489
hayarovy <edurd@rambler.ru>
2023-11-04 14:34:33 +0500

    release SimilarAdapter and SimilarHolder

---
+++ app/src/main/java/ru/practicum/android/diploma/details/presentation/SimilarHolder.kt
@@ -0,0 +1,43 @@
+package ru.practicum.android.diploma.details.presentation
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import ru.practicum.android.diploma.R
+import ru.practicum.android.diploma.databinding.ItemSimilarBinding
+import ru.practicum.android.diploma.details.domain.models.ProfessionSimillar
+import ru.practicum.android.diploma.util.formattedNumber
+
+class SimilarHolder (private val binding: ItemSimilarBinding, private val context: Context):
+    RecyclerView.ViewHolder(binding.root) {
+    @SuppressLint("SetTextI18n")
+    fun bind (item: ProfessionSimillar) {
+        Glide
+            .with(this.itemView.context)
+            .load(item.employerLogo)
+            .placeholder(R.drawable.vacancy_item_search_placeholder)
+            .into(binding.similarLogo)
+        binding.nameSimilar.text = if (item.city!=null) {
+            "${item.name}, ${item.city}"
+        } else {
+            item.name
+        }
+        binding.nameSimilarEmployer.text = item.employerName
+        val from = context.getString(R.string.from)
+        val to = context.getString(R.string.to)
+        val notSalary = context.getString(R.string.not_salary)
+        if (item.salaryFrom == null && item.salaryTo == null) {
+            binding.salarySimilar.text = notSalary
+        } else if (item.salaryFrom != null && item.salaryTo == null) {
+            binding.salarySimilar.text =
+                "$from ${item.salaryFrom.formattedNumber()} ${item.salaryCurrency}"
+        } else if (item.salaryFrom == null && item.salaryTo != null) {
+            binding.salarySimilar.text =
+                "$to ${item.salaryTo.formattedNumber()} ${item.salaryCurrency}"
+        } else if (item.salaryFrom != null && item.salaryTo != null) {
+            binding.salarySimilar.text =
+                "$from ${item.salaryFrom.formattedNumber()} $to ${item.salaryTo.formattedNumber()} ${item.salaryCurrency}"
+        }
+    }
+}
