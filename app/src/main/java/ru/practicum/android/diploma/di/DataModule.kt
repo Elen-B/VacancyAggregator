2e3d6a84cf6cf0026c83d5a5341daf4b96c9f138
alferac@inbox.ru <dmitr-nik.k@yandex.ru>
2023-11-05 12:38:23 +0500

    DIPLOMA-28: add database

--- app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/DataModule.kt
@@ -1,9 +1,11 @@
 package ru.practicum.android.diploma.di
 
+import androidx.room.Room
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import ru.practicum.android.diploma.core.db.AppDatabase
 import ru.practicum.android.diploma.core.network.HhunterApi
 import ru.practicum.android.diploma.core.network.NetworkClient
 import ru.practicum.android.diploma.core.network.RetrofitNetworkClient
@@ -23,5 +25,9 @@ val dataModule = module {
 
     single<NetworkClient> {
         RetrofitNetworkClient(get(), androidContext())
+    }
+
+    single {
+        Room.databaseBuilder(androidContext(), AppDatabase::class.java, "database.db").build()
     }
 }
