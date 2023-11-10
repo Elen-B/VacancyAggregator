af2208cfdb66c5f0357b71bd10658a2312d0cef8
Vasil9202 <118492858+Vasil9202@users.noreply.github.com>
2023-11-10 04:27:58 +0300

    Настроен репозиторий

---
+++ app/src/main/java/ru/practicum/android/diploma/search/data/VacancyPageSource.kt
@@ -0,0 +1,43 @@
+package ru.practicum.android.diploma.search.data
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import ru.practicum.android.diploma.core.network.NetworkClient
+import ru.practicum.android.diploma.core.network.dto.Request
+import ru.practicum.android.diploma.search.data.dto.VacancySearchResponse
+import ru.practicum.android.diploma.search.domain.models.SearchVacancy
+import ru.practicum.android.diploma.util.PAGE
+
+class VacancyPageSource(
+    private val networkClient: NetworkClient,
+    private val option: HashMap<String, String>
+) : PagingSource<Int, SearchVacancy>() {
+
+    override suspend fun load(params: LoadParams<Int>): LoadResult<Int, SearchVacancy> {
+
+        return try {
+            val position = params.key ?: 1
+            option.put(PAGE, position.toString())
+            val response = networkClient.doRequest(
+                Request.VacancySearchRequest(
+                    option
+                )
+            )
+            LoadResult.Page(
+                data = (response as VacancySearchResponse).items.map { it.toSearchVacancy() },
+                prevKey = if (position == 1) null else position - 1,
+                nextKey = position + 1
+            )
+        } catch (e: Exception) {
+            LoadResult.Error(e)
+        }
+
+    }
+
+    override fun getRefreshKey(state: PagingState<Int, SearchVacancy>): Int? {
+        return state.anchorPosition?.let { anchorPosition ->
+            state.closestPageToPosition(anchorPosition)?.prevKey?.plus(1)
+                ?: state.closestPageToPosition(anchorPosition)?.nextKey?.minus(1)
+        }
+    }
+}
--- app/src/main/java/ru/practicum/android/diploma/search/data/dto/VacancySearchResponse.kt
+++ app/src/main/java/ru/practicum/android/diploma/search/data/dto/VacancySearchResponse.kt
@@ -3,5 +3,11 @@ package ru.practicum.android.diploma.search.data.dto
 import ru.practicum.android.diploma.core.data.network.dto.Response
 import ru.practicum.android.diploma.core.data.network.dto.VacancyDTO
 
-class VacancySearchResponse(val items: List<VacancyDTO>, val found: String) : Response(){
+data class VacancySearchResponse(
+    val items: List<VacancyDTO>,
+    val found: String,
+    val page: String?,
+    val pages: String?,
+    val per_page: String?
+) : Response() {
 }
