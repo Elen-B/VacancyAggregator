41ae98346bc53770f9e04c438ac702ffa20774c8
Vasil9202 <118492858+Vasil9202@users.noreply.github.com>
2023-11-19 06:32:08 +0300

    Исправлено окончание количества вакансий

--- app/src/main/java/ru/practicum/android/diploma/util/VacancyCountFormatter.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/VacancyCountFormatter.kt
@@ -7,14 +7,14 @@ class VacancyCountFormatter(private val context: Context) {
 
     fun getStringCount(count: String): String {
         val digit = count.last().toString().toInt()
-        if (count.takeLast(2).toInt() in 10..20) {
-            return context.getString(R.string.found0Vacancies, count)
+        return if (count.takeLast(2).toInt() in 10..20) {
+            context.getString(R.string.found0Vacancies, count)
         } else if (digit == 1) {
-            return context.getString(R.string.found1Vacancies, count)
+            context.getString(R.string.found1Vacancies, count)
         } else if (digit in 2..4) {
-            return context.getString(R.string.found2Vacancies, count)
+            context.getString(R.string.found2Vacancies, count)
         } else {
-            return context.getString(R.string.found0Vacancies, count)
+            context.getString(R.string.found0Vacancies, count)
         }
     }
 }
